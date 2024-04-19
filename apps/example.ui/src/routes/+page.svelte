96a902e38ea894f9947c65ed3807a3a3e79574f2
Marcel Sprada <marcel.sprada@netzkern.de>
2024-04-19 13:32:17 +0200

    - reference shared lib

--- apps/example.ui/src/routes/+page.svelte
+++ apps/example.ui/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+	import { OrderDto } from 'data-access-models';
+	const order: OrderDto = new OrderDto();
+</script>
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
