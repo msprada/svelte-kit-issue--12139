5ab6c60a467534e56192954305045420badee58a
= <=>
2024-06-08 07:42:15 +0200

    - adding more information to html

--- apps/example.ui/src/routes/+page.svelte
+++ apps/example.ui/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { OrderDto } from 'data-access-models';
 	const order: OrderDto = new OrderDto();
+	order.id= "WE67878678"
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit Issue 12139</h1>
+<h3> Here you can see the order object imported from a shared lib:</h3>
+<p> The order id is {order.id}. </p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
